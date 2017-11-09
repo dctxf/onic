b99e75f6745bc302cab400d0159b9de42d572ca6
dctxf <dctmz@qq.com>
2017-11-09 14:23:19 +0800

    v1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: dctxf
  * @Date:   2017-11-08 10:43:01
  * @Last Modified by:   dctxf
- * @Last Modified time: 2017-11-08 17:37:55
+ * @Last Modified time: 2017-11-09 14:19:17
  */
 /**
  * [exports description]
@@ -18,66 +18,59 @@
 const fs = require('fs')
 const rp = require('request-promise')
 const crypto = require('crypto')
-const md5 = crypto.createHash('md5')
+const sortKeys = require('sort-keys')
 const Onic = function (options = {}) {
-  this.gateway = options.gateway
-  this.publicKey = fs.readFileSync(options.publicKey).toString()
-  this.privateKey = fs.readFileSync(options.privateKey).toString()
-
+  this.gateway = options.gateway // 网关地址
+  this.appSerect = options.appSerect
+  this.publicKey = fs.readFileSync(options.publicKey, 'utf8').toString() //公钥
+  this.privateKey = fs.readFileSync(options.privateKey, 'utf8').toString() //私钥
   this.sysInputArg = {
     appKey: options.appKey, //接入方appKey
     format: options.format || 'json', //报文格式
     signType: options.signType || 'RSA', //签名类型，目前支持"RSA""MD5"
     charset: options.charset || 'UTF-8', //字符编码，目前只支持UTF-8
     version: options.version || '1.0.0',
-    encrypt: options.encrypt || 1 //content是否进行RSA加密，如果请求加密，那么响应也是加密的,1加密，0不加密
+    encrypt: options.encrypt !== 0 ? 1 : 0 //content是否进行RSA加密，如果请求加密，那么响应也是加密的,1加密，0不加密
   }
 }
 Onic.prototype.post = function (serviceName, data) {
-  let body = Object.assign({}, this.sysInputArg, {
+  let content = JSON.stringify(sortKeys(data))
+  let body
+  // return this
+  if (this.sysInputArg.encrypt !== 0) {
+    // 对content加密
+    content = crypto.createSign('RSA-SHA1')
+      .update(content)
+      .sign(this.publicKey, 'hex')
+  }
+  body = Object.assign({}, this.sysInputArg, {
     serviceName,
-    content: this.sysInputArg.encrypt === 0 ? this.signer(this.sysInputArg.signType, this.sorter(data)) : this.sorter(data),
+    content,
     timestamp: +new Date()
   })
-  body.sign = this.signer(this.sysInputArg.signType, this.sorter(body))
-  return body
-  const options = {
+  body.sign = this.countersign(body) //加签
+  return Promise.resolve(rp({
     method: 'POST',
     uri: this.gateway,
     headers: {
       'contentType': 'application/json;charset=utf-8'
     },
     json: true,
-    body: body
-  }
-  return rp(options)
+    body: sortKeys(body)
+  }))
 }
-Onic.prototype.signer = function (algorithm, data) {
-  if (algorithm.toLocaleUpperCase() === 'RSA') {
-    algorithm = 'RSA-SHA1'
-    const sign = crypto.createSign(algorithm)
-    sign.update(data);
-    sig = sign.sign(this.privateKey, 'hex');
-    return sig;
-  }
-  return md5.update(data).digest('hex');
-}
-Onic.prototype.sorter = function (data) {
-  let arr = []
+Onic.prototype.countersign = function (data) {
+  data = sortKeys(data)
+  let toBeSign = ''
   for (let k in data) {
-    arr.push({
-      name: k,
-      value: data[k],
-    })
+    toBeSign += ([k] + data[k])
   }
-
-  data = arr.sort((a, b) => {
-    return a.name.charCodeAt() > b.name.charCodeAt()
-  })
-  let obj = {}
-  for (let i = 0; i < data.length; i++) {
-    obj[data[i].name] = data[i].value
+  // return toBeSign
+  switch (this.sysInputArg.signType.toLocaleUpperCase()) {
+    case 'RSA':
+      break
+    default:
+      return crypto.createHash('md5').update(toBeSign + this.appSerect).digest('hex')
   }
-  return JSON.stringify(obj)
 }
 module.exports = Onic
